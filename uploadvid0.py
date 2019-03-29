ea6e309ce1df4f41c480f096c01333a34434f12d
Sergey <rvsl@bk.ru>
2019-03-29 15:55:54 +0300

    fix

--- uploadvid0.py
+++ uploadvid0.py
@@ -4,20 +4,20 @@ from werkzeug.utils import secure_filename
 from flask import send_from_directory
 from werkzeug import SharedDataMiddleware
 
-app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+UPLOAD_FOLDER = 'static/img'
+ALLOWED_EXTENSIONS = set(['wav', 'mp4'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.add_url_rule('static/img/<filename>', 'uploaded_file',
                  build_only=True)
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-    '/uploads':  app.config['UPLOAD_FOLDER']
+    'static/img':  app.config['UPLOAD_FOLDER']
 })
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['wav', 'mp4'])
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
