03b6449ea1db8e73eb86c952217d90f66eaa2622
Sergey <rvsl@bk.ru>
2019-03-29 19:29:09 +0300

    fix

--- flash.py
+++ flash.py
@@ -9,18 +9,19 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-   error = None
+    error = None
 
-   if request.method == 'POST':
+    if request.method == 'POST':
       if request.form['username'] != 'admin' or \
          request.form['password'] != 'admin':
          error = 'Invalid username or password. Please try again!'
       else:
          flash('You were successfully logged in')
-         error = 'You were successfully logged in'
+         #error = 'You were successfully logged in'
          return redirect(url_for('index'))
 
-   return render_template('login.html', error = error)
+    flash('Hello')
+    return render_template('login.html', error = error)
 
 if __name__ == "__main__":
    app.run(debug = True)
